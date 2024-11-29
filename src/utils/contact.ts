72dd33d779597fda3ffc23bc824beff2f820d5b5
Chukwuebuka Chidi-Ezirim <chizim2003@gmail.com>
2024-11-29 13:18:48 +0100

    added  a searchContact method, an addContact method, a deleteContact method, a getContact method

--- src/utils/contact.ts
+++ src/utils/contact.ts
@@ -1,11 +1,11 @@
 import { generateId } from "./generateId";
-class Contact {
+export class Contact {
   id: string;
   name: string;
   email: string;
   phone: string;
 
-  constructor(id: string, name: string, email: string, phone: string) {
+  constructor(name: string, email: string, phone: string) {
     this.id = generateId();
     this.name = name;
     this.email = email;
@@ -13,12 +13,28 @@ class Contact {
   }
 
   getContacts(): Contact[] {
-    const contactsJson = localStorage.getItem('contacts')
-    return contactsJson? JSON.parse(contactsJson) : []
+    const contactsJson = localStorage.getItem("contacts");
+    return contactsJson ? JSON.parse(contactsJson) : [];
   }
 
-  addContact(newContact : Contact):void{
+  addContact(newContact: Contact): void {
+    const contactArray = this.getContacts();
+    contactArray.push(newContact);
+    localStorage.setItem("contacts", JSON.stringify(contactArray));
+  }
+
+  deleteContact(id: string): void {
+    const contactArray = this.getContacts();
+    const updatedContactArray = contactArray.filter(
+      (contact) => contact.id !== id
+    );
+    localStorage.setItem("contacts", JSON.stringify(updatedContactArray));
+  }
+  searchContact(query: string): Contact[]{
     const contactArray = this.getContacts()
-  contactArray.push(newContact)
+    const foundContacts = contactArray.filter((contact) =>(
+      contact.name === query
+    ))
+    return foundContacts
   }
 }
